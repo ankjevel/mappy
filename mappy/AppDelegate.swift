521be55acd2893dd7a18d452b17a134a62975973
Dennis Pettersson <mail@dennispettersson.se>
2015-07-10 00:54:34 +0200

    Initial Commit

---
+++ mappy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mappy
+//
+//  Created by Dennis Pettersson on 2015-07-10.
+//  Copyright (c) 2015 dennisp.se. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ mappy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mappy
+//
+//  Created by Dennis Pettersson on 2015-07-10.
+//  Copyright (c) 2015 dennisp.se. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
